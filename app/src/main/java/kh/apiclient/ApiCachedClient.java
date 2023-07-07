57c6721286af7519f9108d9f0172d71d6cc38dd8
kevinhooke <kevin.hooke@gmail.com>
2023-07-07 16:36:57 -0700

    added serverless config

--- app/src/main/java/kh/apiclient/ApiCachedClient.java
+++ app/src/main/java/kh/apiclient/ApiCachedClient.java
@@ -6,13 +6,16 @@ package kh.apiclient;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class ApiCachedClient implements RequestHandler<Map<String,String>, String>{
 
 	@Override
 	public String handleRequest(Map<String, String> input, Context context) {
-		// TODO Auto-generated method stub
+		LambdaLogger logger = context.getLogger();
+		logger.log("ApiCachedClient invoked");
+		
 		return null;
 	}
 
